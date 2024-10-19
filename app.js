06c973b713e3a2f4a71860acba893ea90337829d
woodylucas <wlucas0311@gmail.com>
2024-10-18 20:35:08 -0400

    feat: nav-bar integrated

--- app.js
+++ app.js
@@ -1,30 +1,62 @@
+const carouselWrapper = document.querySelector(".carousel-wrapper");
+
 const renderCarousel = (wrapper) => {
   const images = wrapper.querySelectorAll(".carousel-image");
+  const carouselNav = document.querySelector(".carousel-nav");
   const leftBtn = document.querySelector(".left");
   const rightBtn = document.querySelector(".right");
 
   const size = images.length;
   let imageWidth = images[0].getBoundingClientRect().width;
   let currentIdx = 0;
+  let navBarButtons = [];
 
   leftBtn.addEventListener("click", handleLeftClick);
   rightBtn.addEventListener("click", handleRightClick);
   window.addEventListener("resize", handleResize);
 
-  function updateCarousel() {
-    // Recalculate the image width dynamically
-    imageWidth = images[0].getBoundingClientRect().width;
-    const translateX = -currentIdx * imageWidth;
-    wrapper.style.transform = `translateX(${translateX}px)`;
+  function createNavButtons(images) {
+    const fragment = document.createDocumentFragment();
+    images.forEach(({ src, alt }, idx) => {
+      const navButtonElement = document.createElement("button");
+      navButtonElement.classList.add("nav-button");
+      navButtonElement.addEventListener("click", () => {
+        currentIdx = idx;
+        updateCarousel();
+        updateNavBarButtons();
+      });
+      fragment.appendChild(navButtonElement);
+    });
+
+    carouselNav.appendChild(fragment);
+    navBarButtons = document.querySelectorAll(".nav-button");
   }
+
+  createNavButtons(images);
+
+  function updateCarousel() {
+    imageWidth = images[0].getBoundingClientRect().width;
+    const shiftHorizontal = -currentIdx * imageWidth;
+    wrapper.style.transform = `translateX(${shiftHorizontal}px)`;
+    updateNavBarButtons();
+  }
+
+  function updateNavBarButtons() {
+    navBarButtons.forEach((button, idx) => {
+      if (idx === currentIdx) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  }
+
   function handleLeftClick() {
     currentIdx = (currentIdx - 1 + size) % size;
-    console.log(currentIdx);
     updateCarousel();
   }
   function handleRightClick() {
     currentIdx = (currentIdx + 1) % size;
-    console.log(currentIdx);
     updateCarousel();
   }
 
@@ -32,13 +64,15 @@ const renderCarousel = (wrapper) => {
     updateCarousel();
   }
 
-  function cleanup() {
+  function cleanUp() {
     window.removeEventListener("resize", handleResize);
     leftBtn.removeEventListener("click", handleLeftClick);
     rightBtn.removeEventListener("click", handleRightClick);
   }
 
-  return { cleanup };
+  updateCarousel();
+
+  return { cleanUp };
 };
 
-const carousel = renderCarousel(document.querySelector(".carousel-wrapper"));
+const carousel = renderCarousel(carouselWrapper);
