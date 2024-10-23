75fffc061f7142360ee7f4876c67f05ac2db393c
woodylucas <wlucas0311@gmail.com>
2024-10-23 16:09:13 -0400

    feat: updated carousel added border

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-const carouselWrapper = document.querySelector(".carousel-wrapper");
+const wrapper = document.querySelector(".carousel-wrapper");
 
-const renderCarousel = (wrapper) => {
+const createCarousel = (wrapper) => {
   const images = wrapper.querySelectorAll(".carousel-image");
   const carouselNav = document.querySelector(".carousel-nav");
-  const leftBtn = document.querySelector(".left");
-  const rightBtn = document.querySelector(".right");
+  const leftButton = document.querySelector(".left");
+  const rightButton = document.querySelector(".right");
 
+  let imagesWidth = images[0].getBoundingClientRect().width;
   const size = images.length;
-  let imageWidth = images[0].getBoundingClientRect().width;
   let currentIdx = 0;
   let navBarButtons = [];
 
-  leftBtn.addEventListener("click", handleLeftClick);
-  rightBtn.addEventListener("click", handleRightClick);
-  window.addEventListener("resize", handleResize);
+  leftButton.addEventListener("click", handleLeftButton);
+  rightButton.addEventListener("click", handleRightButton);
+  window.addEventListener("resize", () => updateCarousel());
 
-  function createNavButtons(images) {
+  function updateCarousel() {
+    imagesWidth = images[0].getBoundingClientRect().width;
+    const shiftHorizonally = -currentIdx * imagesWidth;
+    wrapper.style.transform = `translateX(${shiftHorizonally}px)`;
+  }
+
+  function createNavBarButtons(images) {
+    carouselNav.replaceChildren();
     const fragment = document.createDocumentFragment();
-    images.forEach(({ src, alt }, idx) => {
-      const navButtonElement = document.createElement("button");
-      navButtonElement.classList.add("nav-button");
-      navButtonElement.addEventListener("click", () => {
+    images.forEach((image, idx) => {
+      const navBarButton = document.createElement("button");
+      navBarButton.classList.add("nav-button");
+      navBarButton.addEventListener("click", () => {
         currentIdx = idx;
         updateCarousel();
         updateNavBarButtons();
       });
-      fragment.appendChild(navButtonElement);
+      fragment.appendChild(navBarButton);
     });
 
     carouselNav.appendChild(fragment);
-    navBarButtons = document.querySelectorAll(".nav-button");
+    navBarButtons = carouselNav.querySelectorAll(".nav-button");
   }
 
-  createNavButtons(images);
-
-  function updateCarousel() {
-    imageWidth = images[0].getBoundingClientRect().width;
-    const shiftHorizontal = -currentIdx * imageWidth;
-    wrapper.style.transform = `translateX(${shiftHorizontal}px)`;
-    updateNavBarButtons();
-  }
+  createNavBarButtons(images);
 
   function updateNavBarButtons() {
     navBarButtons.forEach((button, idx) => {
-      if (idx === currentIdx) {
+      if (currentIdx === idx) {
         button.classList.add("active");
       } else {
         button.classList.remove("active");
@@ -51,28 +51,19 @@ const renderCarousel = (wrapper) => {
     });
   }
 
-  function handleLeftClick() {
+  function handleLeftButton() {
     currentIdx = (currentIdx - 1 + size) % size;
     updateCarousel();
+    updateNavBarButtons();
   }
-  function handleRightClick() {
+  function handleRightButton() {
     currentIdx = (currentIdx + 1) % size;
     updateCarousel();
+    updateNavBarButtons();
   }
 
-  function handleResize() {
-    updateCarousel();
-  }
-
-  function cleanUp() {
-    window.removeEventListener("resize", handleResize);
-    leftBtn.removeEventListener("click", handleLeftClick);
-    rightBtn.removeEventListener("click", handleRightClick);
-  }
-
+  updateNavBarButtons();
   updateCarousel();
-
-  return { cleanUp };
 };
 
-const carousel = renderCarousel(carouselWrapper);
+const carousel = createCarousel(wrapper);
